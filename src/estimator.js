87bbc6dee47824ab080820c3f20ccad8cf4cb79a
kiborgok <borgokalek14@gmail.com>
2020-04-09 12:43:05 +0300

    Edited challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -4,13 +4,10 @@ const covid19ImpactEstimator = (data) => {
     switch (period) {
       case 'days':
         return timeToElapse;
-        break;
       case 'weeks':
         return timeToElapse * 7;
-        break;
       case 'months':
         return timeToElapse * 30;
-        break;
       default:
         return null;
     }
@@ -21,18 +18,18 @@ const covid19ImpactEstimator = (data) => {
 
   const crntlyInfected = currentlyInfected(data.reportedCases);
   const bedAvailability = data.totalHospitalBeds * 0.35;
-  const period = normalizePeriod(data.periodType, data.timeToElapse)
+  const period = normalizePeriod(data.periodType, data.timeToElapse);
 
   impact.currentlyInfected = crntlyInfected * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * period;
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
-  impact.hospitalBedsByRequestedTime = bedAvailability - impact.severeCasesByRequestedTime | 0;
+  impact.hospitalBedsByRequestedTime = perseInt(bedAvailability - impact.severeCasesByRequestedTime);
 
   severeImpact.currentlyInfected = crntlyInfected * 50;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * period;
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
   const severeImpactCasesByRequestedTime = severeImpact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime = bedAvailability - severeImpactCasesByRequestedTime | 0;
+  severeImpact.hospitalBedsByRequestedTime = perseInt(bedAvailability - severeImpactCasesByRequestedTime);
 
   return {
     data,
